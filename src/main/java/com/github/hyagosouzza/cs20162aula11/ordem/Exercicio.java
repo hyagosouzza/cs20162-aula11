495e6a291cf398e901c0529578060f6fd6fc95d9
hyagosouzza <hyagosouzza@hotmail.com>
2016-10-26 21:40:01 -0200

    Update Exercicio.java

--- src/main/java/com/github/hyagosouzza/cs20162aula11/ordem/Exercicio.java
+++ src/main/java/com/github/hyagosouzza/cs20162aula11/ordem/Exercicio.java
@@ -1,24 +1,21 @@
 package com.github.hyagosouzza.cs20162aula11.ordem;
 
 import com.github.kyriosdata.parser.Expressao;
-import com.github.kyriosdata.parser.IParser;
 import com.github.kyriosdata.parser.Lexer;
 import com.github.kyriosdata.parser.Parser;
 import com.github.kyriosdata.parser.Token;
+import java.util.Scanner;
 import java.util.List;
 
 public class Exercicio {
     
     public static void main (String[] args) {
+        System.out.print("Digite uma expressão: ");
+        Scanner ler = new Scanner(System.in);
+        String frase = new.nextLine();
         throw new Exception("Erro!");
         try{
-        System.out.println(exprPara("1 + 1").valor());
-        } catch (Exception ex) {
-            System.out.println(ex.message());
-        }
-
-        try{
-        System.out.println(exprPara(args[0]).valor());
+        System.out.println(exprPara(frase).valor());
         } catch (Exception ex) {
             System.out.println(ex.message());
         }
