16db6ea3bb79dcf06bc0710e3223b834d4d9a335
hyagosouzza <hyagosouzza@hotmail.com>
2016-11-30 20:14:45 -0200

    Delete package-info.java

--- package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Pacote : com.github.hyagosouzza.cs20162aula11.ordem.
- * Pacote que contém os métodos para verificar uma expressão
- * matemática e retornar o seu valor para o usuário, se
- * possível.
- * @version 1.0
- */
-package com.github.hyagosouzza.cs20162aula11.ordem;
